c144d4db3e96a8052adb66ac575cb1913ec3efc2
“orodriguez” <omarjavier15@gmail.com>
2024-02-11 12:59:28 -0400

    Stack tests

---
+++ AAD.Tests/StackTests.cs
@@ -0,0 +1,46 @@
+using System.Collections;
+
+namespace AAD.Tests;
+
+public class StackTests
+{
+    [Fact]
+    public void Pop()
+    {
+        var stack = new Stack<string>();
+        
+        stack.Push("A");
+        stack.Push("B");
+        
+        Assert.Equal("B", stack.Pop());
+    }
+    
+    [Fact]
+    public void Pop_Empty()
+    {
+        var stack = new Stack<string>();
+
+        Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+
+    [Fact]
+    public void Peek()
+    {
+        var stack = new Stack<int>();
+        stack.Push(1);
+        
+        var result = stack.Peek();
+        
+        Assert.Equal(result, 1);
+    }
+    
+    [Fact]
+    public void Peek_Empty()
+    {
+        var stack = new Stack<int>();
+        
+        var result = stack.Peek();
+        
+        Assert.Throws<InvalidOperationException>(() => stack.Pop());
+    }
+}
